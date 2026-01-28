f93d07092bf511b454af6b82c663dc023c45b24e
kyriacos1999-cloud <kyriacos1999@gmail.com>
2026-01-28 18:51:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Toaster } from "./components/ui/toaster"
+import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Hearts from "./pages/Hearts";
 import HeartShare from "./pages/HeartShare";
+import Thanks from "./pages/Thanks";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/hearts" element={<Hearts />} />
           <Route path="/heart/:id" element={<HeartShare />} />
+          <Route path="/thanks" element={<Thanks />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
